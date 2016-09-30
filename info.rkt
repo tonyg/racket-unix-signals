7231d17617a013da8f0b057f479f3c189d56daf6
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-09-30 12:52:24 -0400

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang setup/infotab
 (define collection 'multi)
-(define deps '("base" "rackunit-lib"))
+(define deps '("base" "rackunit-lib" "make"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define homepage "https://github.com/tonyg/racket-unix-signals")
